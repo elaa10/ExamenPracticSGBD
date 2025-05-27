96a93245cd5b1879547dc07bc7d8ec90dcdd81fc
elaa10 <alexandraisa17@yahoo.com>
2025-05-27 06:54:40 +0300

    Primul commit - proiect Examen Practic

---
+++ ExamenPractic/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ExamenPractic
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            dataGridViewParent = new DataGridView();
+            dataGridViewChild = new DataGridView();
+            txtNumeBriosa = new TextBox();
+            txtDescriere = new TextBox();
+            numPret = new NumericUpDown();
+            btnAddChild = new Button();
+            btnDeleteChild = new Button();
+            btnUpdateChild = new Button();
+            btnRefresh = new Button();
+
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numPret).BeginInit();
+            SuspendLayout();
+
+            // 
+            // dataGridViewParent
+            // 
+            dataGridViewParent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewParent.Location = new Point(38, 103);
+            dataGridViewParent.Name = "dataGridViewParent";
+            dataGridViewParent.RowHeadersWidth = 51;
+            dataGridViewParent.Size = new Size(300, 188);
+            dataGridViewParent.TabIndex = 0;
+
+            // 
+            // dataGridViewChild
+            // 
+            dataGridViewChild.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewChild.Location = new Point(433, 103);
+            dataGridViewChild.Name = "dataGridViewChild";
+            dataGridViewChild.RowHeadersWidth = 51;
+            dataGridViewChild.Size = new Size(300, 188);
+            dataGridViewChild.TabIndex = 1;
+
+            // 
+            // txtNumeBriosa
+            // 
+            txtNumeBriosa.Location = new Point(63, 321);
+            txtNumeBriosa.Name = "txtNumeBriosa";
+            txtNumeBriosa.Size = new Size(125, 27);
+            txtNumeBriosa.TabIndex = 2;
+            txtNumeBriosa.PlaceholderText = "Nume Brioșă";
+
+            // 
+            // txtDescriere
+            // 
+            txtDescriere.Location = new Point(63, 366);
+            txtDescriere.Name = "txtDescriere";
+            txtDescriere.Size = new Size(125, 27);
+            txtDescriere.TabIndex = 3;
+            txtDescriere.PlaceholderText = "Descriere";
+
+            // 
+            // numPret
+            // 
+            numPret.DecimalPlaces = 2;
+            numPret.Location = new Point(466, 321);
+            numPret.Maximum = 1000;
+            numPret.Name = "numPret";
+            numPret.Size = new Size(150, 27);
+            numPret.TabIndex = 4;
+
+            // 
+            // btnAddChild
+            // 
+            btnAddChild.Location = new Point(185, 42);
+            btnAddChild.Name = "btnAddChild";
+            btnAddChild.Size = new Size(94, 29);
+            btnAddChild.TabIndex = 5;
+            btnAddChild.Text = "Add";
+            btnAddChild.UseVisualStyleBackColor = true;
+
+            // 
+            // btnDeleteChild
+            // 
+            btnDeleteChild.Location = new Point(433, 42);
+            btnDeleteChild.Name = "btnDeleteChild";
+            btnDeleteChild.Size = new Size(94, 29);
+            btnDeleteChild.TabIndex = 6;
+            btnDeleteChild.Text = "Delete";
+            btnDeleteChild.UseVisualStyleBackColor = true;
+
+            // 
+            // btnUpdateChild
+            // 
+            btnUpdateChild.Location = new Point(600, 42);
+            btnUpdateChild.Name = "btnUpdateChild";
+            btnUpdateChild.Size = new Size(94, 29);
+            btnUpdateChild.TabIndex = 7;
+            btnUpdateChild.Text = "Update";
+            btnUpdateChild.UseVisualStyleBackColor = true;
+
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(38, 42);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(94, 29);
+            btnRefresh.TabIndex = 8;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRefresh);
+            Controls.Add(btnUpdateChild);
+            Controls.Add(btnDeleteChild);
+            Controls.Add(btnAddChild);
+            Controls.Add(numPret);
+            Controls.Add(txtDescriere);
+            Controls.Add(txtNumeBriosa);
+            Controls.Add(dataGridViewChild);
+            Controls.Add(dataGridViewParent);
+            Name = "Form1";
+            Text = "Examen Practic";
+
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numPret).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewParent;
+        private DataGridView dataGridViewChild;
+        private TextBox txtNumeBriosa;
+        private TextBox txtDescriere;
+        private NumericUpDown numPret;
+        private Button btnAddChild;
+        private Button btnDeleteChild;
+        private Button btnUpdateChild;
+        private Button btnRefresh;
+    }
+}
---
+++ ExamenPractic/Form1.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace ExamenPractic
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter daParent, daChild;
+        DataSet ds = new DataSet();
+        BindingSource bsParent = new BindingSource();
+        BindingSource bsChild = new BindingSource();
+        string connectionString = @"Server=DELFI\SQLEXPRESS;Database=Problema1;Integrated Security=true; TrustServerCertificate=true;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            btnAddChild.Click += btnAddChild_Click;
+            btnDeleteChild.Click += btnDeleteChild_Click;
+            btnRefresh.Click += btnRefresh_Click;
+            btnUpdateChild.Click += btnUpdateChild_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    daParent = new SqlDataAdapter("SELECT * FROM Cofetarii;", con);
+                    daChild = new SqlDataAdapter("SELECT * FROM Briose;", con);
+
+                    daParent.Fill(ds, "Cofetarii");
+                    daChild.Fill(ds, "Briose");
+
+                    DataColumn pkColumn = ds.Tables["Cofetarii"].Columns["cod_cofetarie"];
+                    DataColumn fkColumn = ds.Tables["Briose"].Columns["cod_cofetarie"];
+
+                    DataRelation relation = new DataRelation("FK_Cofetarii_Briose", pkColumn, fkColumn);
+                    ds.Relations.Add(relation);
+
+                    bsParent.DataSource = ds.Tables["Cofetarii"];
+                    dataGridViewParent.DataSource = bsParent;
+
+                    bsChild.DataSource = bsParent;
+                    bsChild.DataMember = "FK_Cofetarii_Briose";
+                    dataGridViewChild.DataSource = bsChild;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la incarcarea datelor: " + ex.Message);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    daParent.SelectCommand.Connection = con;
+                    daChild.SelectCommand.Connection = con;
+
+                    if (ds.Tables.Contains("Briose")) ds.Tables["Briose"].Clear();
+                    if (ds.Tables.Contains("Cofetarii")) ds.Tables["Cofetarii"].Clear();
+
+                    daParent.Fill(ds, "Cofetarii");
+                    daChild.Fill(ds, "Briose");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la reincarcarea datelor: " + ex.Message);
+            }
+        }
+
+        private void btnAddChild_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNumeBriosa.Text))
+                {
+                    MessageBox.Show("Numele brioșei nu poate fi gol.");
+                    return;
+                }
+
+                if (numPret.Value <= 0)
+                {
+                    MessageBox.Show("Prețul trebuie să fie mai mare decât 0.");
+                    return;
+                }
+
+                DataRow selectedRow = ((DataRowView)bsParent.Current).Row;
+                int codCofetarie = Convert.ToInt32(selectedRow["cod_cofetarie"]);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "INSERT INTO Briose (cod_cofetarie, nume_briosa, descriere, pret) " +
+                        "VALUES (@cod_cofetarie, @nume_briosa, @descriere, @pret)", con);
+
+                    cmd.Parameters.AddWithValue("@cod_cofetarie", codCofetarie);
+                    cmd.Parameters.AddWithValue("@nume_briosa", txtNumeBriosa.Text);
+                    cmd.Parameters.AddWithValue("@descriere", txtDescriere.Text);
+                    cmd.Parameters.AddWithValue("@pret", Convert.ToDouble(numPret.Value));
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Brioșa a fost adăugată cu succes!");
+                    btnRefresh_Click(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la adăugarea brioșei: " + ex.Message);
+            }
+        }
+
+        private void btnDeleteChild_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bsChild.Current != null)
+                {
+                    DataRow selectedRow = ((DataRowView)bsChild.Current).Row;
+                    if (selectedRow["cod_briosa"] == DBNull.Value)
+                    {
+                        MessageBox.Show("ID invalid.");
+                        return;
+                    }
+
+                    int codBriosa = Convert.ToInt32(selectedRow["cod_briosa"]);
+
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+
+                        SqlCommand cmd = new SqlCommand("DELETE FROM Briose WHERE cod_briosa = @cod_briosa", con);
+                        cmd.Parameters.AddWithValue("@cod_briosa", codBriosa);
+                        cmd.ExecuteNonQuery();
+
+                        btnRefresh_Click(sender, e);
+                        MessageBox.Show("Brioșa a fost ștearsă cu succes!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nu ați selectat nicio brioșă pentru ștergere.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la ștergerea brioșei: " + ex.Message);
+            }
+        }
+
+        private void btnUpdateChild_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bsChild.Current != null)
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        DataRow selectedRow = ((DataRowView)bsChild.Current).Row;
+
+                        if (selectedRow["cod_briosa"] == DBNull.Value)
+                        {
+                            MessageBox.Show("ID invalid.");
+                            return;
+                        }
+
+                        daChild.UpdateCommand = new SqlCommand(
+                            "UPDATE Briose SET cod_cofetarie = @cod_cofetarie, nume_briosa = @nume_briosa, descriere = @descriere, pret = @pret " +
+                            "WHERE cod_briosa = @cod_briosa", con);
+
+                        daChild.UpdateCommand.Parameters.AddWithValue("@cod_cofetarie", Convert.ToInt32(selectedRow["cod_cofetarie"]));
+                        daChild.UpdateCommand.Parameters.AddWithValue("@cod_briosa", Convert.ToInt32(selectedRow["cod_briosa"]));
+                        daChild.UpdateCommand.Parameters.AddWithValue("@nume_briosa", selectedRow["nume_briosa"].ToString());
+                        daChild.UpdateCommand.Parameters.AddWithValue("@descriere", selectedRow["descriere"].ToString());
+                        daChild.UpdateCommand.Parameters.AddWithValue("@pret", Convert.ToDouble(selectedRow["pret"]));
+
+                        int rowsAffected = daChild.UpdateCommand.ExecuteNonQuery();
+                        if (rowsAffected >= 1)
+                        {
+                            MessageBox.Show("Brioșa a fost actualizată cu succes!");
+                        }
+
+                        btnRefresh_Click(sender, e);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nu ați selectat nicio brioșă pentru actualizare.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la actualizarea brioșei: " + ex.Message);
+            }
+        }
+    }
+}
